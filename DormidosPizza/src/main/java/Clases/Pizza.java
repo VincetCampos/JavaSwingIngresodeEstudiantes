4d03bd0b287f39877a3d795125820293932c5283
VincetCampos <62453298+VincetCampos@users.noreply.github.com>
2021-06-09 17:10:58 -0600

    Se completo la ventana de Administracion

---
+++ DormidosPizza/src/main/java/Clases/Pizza.java
@@ -0,0 +1,58 @@
+package Clases;
+/**
+ *
+ * @author PC1
+ */
+public class Pizza {
+    private int id;
+    private String sucursal;
+    private String nombre;
+    private String precio;
+    private String descripcion;
+
+    public Pizza() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(String sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    
+}
